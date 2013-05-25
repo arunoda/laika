6ef13cbb624a12fee4fa26c4ea4e4e7dda95c76e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-25 13:58:10 +0530

    speedup testing with the use of app pooling

--- examples/hello/tests/posts.js
+++ examples/hello/tests/posts.js
@@ -1,7 +1,23 @@
 var assert = require('assert');
 
 suite('Posts', function() {
+  test('server only', laika(function(done, server) {
+    // done();
+    server.eval(function() {
+      Posts.insert({a: 100});
+      Posts.insert({b: 200});
+      var docs = Posts.find().fetch();
+      emit('doc', docs)
+    });
+
+    server.once('doc', function(docs) {
+      assert.equal(docs.length, 2);
+      done();
+    });
+  }));
+
   test('insert and observe', laika(function(done, server, client) {
+    // done();
     server.eval(function() {
       Posts.remove({});
       Posts.find().observe({
@@ -13,7 +29,7 @@ suite('Posts', function() {
       }
     });
 
-    server.on('doc', function(doc) {
+    server.once('doc', function(doc) {
       delete doc._id;
       assert.deepEqual(doc, {a: 10});
       done();
@@ -22,6 +38,8 @@ suite('Posts', function() {
     client.eval(function() {
       Posts.insert({a: 10});
     });
+
+    client.once('done', done);
   }));
 
   test('insert in client and observe in client too', laika(function(done, server, c1, c2) {
@@ -29,7 +47,7 @@ suite('Posts', function() {
     server.eval(function() {
       Posts.remove({});
       emit('done');
-    }).on('done', function() {
+    }).once('done', function() {
       c1.eval(observePosts);
     })
 
@@ -45,7 +63,7 @@ suite('Posts', function() {
       emit('done');
     }
 
-    c1.on('done', function() {
+    c1.once('done', function() {
       c2.eval(insertDoc, insertObject);
     });
 
@@ -53,7 +71,7 @@ suite('Posts', function() {
       Posts.insert(obj);
     }
 
-    c1.on('doc', function(doc) {
+    c1.once('doc', function(doc) {
       assert.equal(doc.k, insertObject.k);
       done();
     });
@@ -71,7 +89,7 @@ suite('Posts', function() {
       }
     });
 
-    server.on('doc', function(doc) {
+    server.once('doc', function(doc) {
       delete doc._id;
       assert.deepEqual(doc, {a: 10});
       done();
