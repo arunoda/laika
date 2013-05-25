7a591b16d39ccf844174445c5aeb83e813032908
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-25 17:14:17 +0530

    added simple testing interface

--- examples/hello/tests/posts.js
+++ examples/hello/tests/posts.js
@@ -1,48 +1,7 @@
 var assert = require('assert');
 
 suite('Posts', function() {
-  test('server only', laika(function(done, server) {
-    // done();
-    server.eval(function() {
-      Posts.insert({a: 100});
-      Posts.insert({b: 200});
-      var docs = Posts.find().fetch();
-      emit('doc', docs)
-    });
-
-    server.once('doc', function(docs) {
-      assert.equal(docs.length, 2);
-      done();
-    });
-  }));
-
-  test('insert and observe', laika(function(done, server, client) {
-    // done();
-    server.eval(function() {
-      Posts.remove({});
-      Posts.find().observe({
-        added: notifyTest
-      })
-
-      function notifyTest(doc) {
-        emit('doc', doc);
-      }
-    });
-
-    server.once('doc', function(doc) {
-      delete doc._id;
-      assert.deepEqual(doc, {a: 10});
-      done();
-    });
-
-    client.eval(function() {
-      Posts.insert({a: 10});
-    });
-
-    client.once('done', done);
-  }));
-
-  test('insert in client and observe in client too', laika(function(done, server, c1, c2) {
+  ltest('insert in client and observe in client too', function(done, server, c1, c2) {
     var insertObject = { k: Math.random() };
     server.eval(function() {
       Posts.remove({});
@@ -75,9 +34,9 @@ suite('Posts', function() {
       assert.equal(doc.k, insertObject.k);
       done();
     });
-  }));
+  });
 
-  test('insert and observe2', laika(function(done, server, client) {
+  ltest('insert and observe2', function(done, server, client) {
     server.eval(function() {
       Posts.remove({});
       Posts.find().observe({
@@ -98,5 +57,5 @@ suite('Posts', function() {
     client.eval(function() {
       Posts.insert({a: 10});
     });
-  }));
+  });
 });
