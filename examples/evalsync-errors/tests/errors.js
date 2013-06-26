b30cda10a2156e90f6890579ba90a0cb9e0e05d6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-06-27 04:29:18 +0530

    added evalSyncExpcectError refactored evalSync specific codebase

--- examples/evalsync-errors/tests/errors.js
+++ examples/evalsync-errors/tests/errors.js
@@ -1,29 +1,75 @@
 var assert = require('assert');
 
-suite('EvalSync Error Catch', function() {
-  test('catch error server', function(done, server, client) {
-    try{
+suite('EvalSync Expect Error', function() {
+  suite('Server:', function() {
+    test('throw normal error', function(done, server, client) {
       server.evalSync(function() {
-        throw new Error('hoiya');
-        emit('return');
-      }); 
-      assert.fail('should throw some error');
-    } catch(ex) {
-      assert.ok(ex.message.match(/hoiya/));
-      done();
-    }
-  });
+        emit('return', false);
+      });
 
-  test('catch error client', function(done, server, client) {
-    try{
-      client.evalSync(function() {
-        throw new Error('hoiya');
+      server.evalSync(function() {
+        throw new Error('normal error');
         emit('return');
       }); 
-      assert.fail('should throw an error');
-    } catch(ex) {
-      assert.ok(ex.message.match(/hoiya/));
+
       done();
-    }
-  });
+    });
+
+    test('throw error on evalSync', function(done, server, client) {
+
+      var error = server.evalSyncExpcectError('I want some error', function() {
+        throw new Error('hoiya');
+        emit('return');
+      });
+      assert.ok(error.message.match(/hoiya/));
+
+      done();
+    });
+
+    test('evalSync Error expected but not thrown', function(done, server, client) {
+
+      server.evalSyncExpcectError('expected but not thrown', function() {
+        // throw new Error('hoiya');
+        emit('return');
+      });
+
+      done();
+    });
+  })
+
+  suite('Client:', function() {
+    test('throw normal error', function(done, server, client) {
+      client.evalSync(function() {
+        emit('return', false);
+      });
+
+      client.evalSync(function() {
+        throw new Error('normal error');
+        emit('return');
+      }); 
+
+      done();
+    });
+
+    test('throw error on evalSync', function(done, server, client) {
+
+      var error = client.evalSyncExpcectError('I want some error', function() {
+        throw new Error('hoiya');
+        emit('return');
+      });
+      assert.ok(error.message.match(/hoiya/));
+
+      done();
+    });
+
+    test('evalSync Error expected but not thrown', function(done, server, client) {
+
+      server.evalSyncExpcectError('expected but not thrown', function() {
+        // throw new Error('hoiya');
+        emit('return');
+      });
+
+      done();
+    });
+  })
 });
