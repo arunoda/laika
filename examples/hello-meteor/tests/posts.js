02c53a6e31812e7805cca1f668eebf7f496004d0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-03 15:14:04 +0530

    added option(-d) to see log outputs from the server/client

--- examples/hello-meteor/tests/posts.js
+++ examples/hello-meteor/tests/posts.js
@@ -1,43 +1,46 @@
 var assert = require('assert');
 
 suite('Posts', function() {
-  ltest('insert in client and observe in client too', function(done, server, c1, c2) {
-    var insertObject = { k: Math.random() };
+  // test('insert in client and observe in client too', function(done, server, c1, c2) {
+  //   var insertObject = { k: Math.random() };
+  //   server.eval(function() {
+  //     console.log('some server message');
+  //     console.error('some server debug');
+  //     Posts.remove({});
+  //     emit('done');
+  //   }).once('done', function() {
+  //     c1.eval(observePosts);
+  //   })
+
+  //   function observePosts() {
+  //     Posts.find().observe({
+  //       added: onAdded
+  //     });
+
+  //     function onAdded(doc) {
+  //       emit('doc', doc);
+  //     }
+
+  //     emit('done');
+  //   }
+
+  //   c1.once('done', function() {
+  //     c2.eval(insertDoc, insertObject);
+  //   });
+
+  //   function insertDoc(obj) {
+  //     Posts.insert(obj);
+  //   }
+
+  //   c1.once('doc', function(doc) {
+  //     assert.equal(doc.k, insertObject.k);
+  //     done();
+  //   });
+  // });
+
+  test('insert and observe2', function(done, server, client) {
     server.eval(function() {
-      Posts.remove({});
-      emit('done');
-    }).once('done', function() {
-      c1.eval(observePosts);
-    })
-
-    function observePosts() {
-      Posts.find().observe({
-        added: onAdded
-      });
-
-      function onAdded(doc) {
-        emit('doc', doc);
-      }
-
-      emit('done');
-    }
-
-    c1.once('done', function() {
-      c2.eval(insertDoc, insertObject);
-    });
-
-    function insertDoc(obj) {
-      Posts.insert(obj);
-    }
-
-    c1.once('doc', function(doc) {
-      assert.equal(doc.k, insertObject.k);
-      done();
-    });
-  });
-
-  ltest('insert and observe2', function(done, server, client) {
-    server.eval(function() {
+      console.log('some server message');
       Posts.remove({});
       Posts.find().observe({
         added: notifyTest
@@ -55,32 +58,34 @@ suite('Posts', function() {
     });
 
     client.eval(function() {
+      console.log('some client message');
+      console.log('some client message2');
       Posts.insert({a: 10});
     });
   });
 
-  ltest('insert in client and observe in client too using evalSync()', function(done, server, c1, c2) {
-    var insertObject = { k: Math.random() };
-    c1.evalSync(function() {
-      Posts.find().observe({
-        added: onAdded
-      });
+  // test('insert in client and observe in client too using evalSync()', function(done, server, c1, c2) {
+  //   var insertObject = { k: Math.random() };
+  //   c1.evalSync(function() {
+  //     Posts.find().observe({
+  //       added: onAdded
+  //     });
 
-      function onAdded(doc) {
-        emit('doc', doc);
-      }
+  //     function onAdded(doc) {
+  //       emit('doc', doc);
+  //     }
 
-      emit('return');
-    });
+  //     emit('return');
+  //   });
 
-    c1.once('doc', function(doc) {
-      assert.equal(doc.k, insertObject.k);
-      done();
-    });
+  //   c1.once('doc', function(doc) {
+  //     assert.equal(doc.k, insertObject.k);
+  //     done();
+  //   });
 
-    c2.eval(function(obj) {
-      Posts.insert(obj);
-    }, insertObject);
+  //   c2.eval(function(obj) {
+  //     Posts.insert(obj);
+  //   }, insertObject);
 
-  });
+  // });
 });
